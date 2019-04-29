b33c9c06aed83eafa8fdc313319de11011add534
kenkuts <kennethchoi19@gmail.com>
2019-04-29 18:06:09 -0400

    brushing up on create action in rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(first_name: "Kenneth", last_name: "Kutschera")
+Student.create(first_name: "John", last_name: "Woods")
+Student.create(first_name: "Awesome", last_name: "Sauce")
+Student.create(first_name: "LeBron", last_name: "James")
